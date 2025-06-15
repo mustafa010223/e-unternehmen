19e3231d41edb421ebc88c4248daddf2c1bf3aef
mustafa010223 <mustafaasalik@gmail.com>
2025-06-15 11:28:15 +0200

    Frontend Dockerfile izin hatası düzeltmeleri ve Dockerfile iyileştirmeleri

---
+++ e-unternehmen-final/frontend/src/App.js
@@ -0,0 +1,14 @@
+import './App.css';
+
+function App() {
+  return (
+    <div className="App">
+      <header className="App-header">
+        <h1>Merhaba K3s CI/CD!</h1>
+        <p>Frontend başarıyla çalışıyor.</p>
+      </header>
+    </div>
+  );
+}
+
+export default App;
