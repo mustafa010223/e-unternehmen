93958184992af133982f94d5f63af4d9ace2b3e6
mustafa010223 <mustafaasalik@gmail.com>
2025-06-15 12:22:52 +0200

    Frontend App.test.js baslik testi guncellendi

--- frontend/src/App.test.js
+++ frontend/src/App.test.js
@@ -3,7 +3,7 @@ import App from './App';
 
 test('renders the main heading', () => {
   render(<App />);
-  // Ekranda "Merhaba K3s CI/CD!" metnini içeren elementi arıyoruz.
+  // Ekranda "Mustafa'nın Mağazası" metnini içeren elementi arıyoruz.
   const headingElement = screen.getByText(/Merhaba K3s CI\/CD/i);
   expect(headingElement).toBeInTheDocument();
 });
